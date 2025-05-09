7762b6b14fa624f53ff0a1339094c65d05d67115
Mykola Melnichuk <nikolfutdeveloper@gmail.com>
2025-05-09 14:59:30 +0200

    final commit

--- src/language/18n.js
+++ src/language/18n.js
@@ -1,22 +1,25 @@
-import i18n from 'i18next'
-import LanguageDetector from 'i18next-browser-languagedetector'
-import Backend from 'i18next-http-backend'
-import { initReactI18next } from 'react-i18next'
+import i18n from "i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import Backend from "i18next-http-backend";
+import { initReactI18next } from "react-i18next";
 
 i18n
   .use(Backend)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    fallbackLng: 'ua',
+    fallbackLng: "ua",
     debug: true,
     detection: {
-      order: ['queryString', 'cookie'],
-      cache: ['cookie'],
+      order: ["queryString", "cookie"],
+      cache: ["cookie"],
+    },
+    backend: {
+      loadPath: "/warapp-react/locales/{{lng}}/{{ns}}.json",
     },
     interpolation: {
       escapeValue: false,
     },
-  })
+  });
 
-export default i18n
+export default i18n;
